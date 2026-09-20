daddf04fb48a4bca04350c23e427788efd95ea10
Sebas0914 <147787208+Sebas0914@users.noreply.github.com>
2026-09-20 15:34:54 -0700

    phase1: add platform abstraction

---
+++ great_sage/core/platform/__init__.py
@@ -0,0 +1,8 @@
+"""Platform abstractions for Great Sage."""
+from .base import AppLauncher, DataPaths, Hotkey, WindowInfo
+from .linux_wayland import LinuxWaylandPlatform
+from .linux_x11 import LinuxX11Platform
+from .windows import WindowsPlatform
+
+__all__ = ["AppLauncher", "DataPaths", "Hotkey", "WindowInfo",
+           "LinuxWaylandPlatform", "LinuxX11Platform", "WindowsPlatform"]
